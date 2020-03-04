85dd7da0cd6478d2e2142976de762bce19cfa282
kansas <xixixi@666.com>
2020-03-04 23:10:12 +0800

    118

---
+++ Easy/118.杨辉三角.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func generate(numRows int) [][]int {
+
+	res := make([][]int, numRows)
+	res[0] = []int{1}
+	for i:=1 ; i <numRows; i++{
+		res[i] = make([]int,i+1)
+		res[i][0] = 1
+		res[i][i] = 1
+		for j := 1; j<i ; j++{
+			res[i][j] = res[i-1][j] + res[i-1][j]
+
+		}
+	}
+	return res
+}
+func main() {
+	fmt.Println(generate(5))
+	
+}
