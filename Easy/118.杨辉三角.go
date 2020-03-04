e6d86ca1447822c33f746cf8eaf4153dc085e650
kansas <xixixi@666.com>
2020-03-04 23:17:33 +0800

    修改118

--- Easy/118.杨辉三角.go
+++ Easy/118.杨辉三角.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func generate(numRows int) [][]int {
 
+	if numRows ==0{
+		return nil
+	}
 	res := make([][]int, numRows)
 	res[0] = []int{1}
 	for i:=1 ; i <numRows; i++{
@@ -11,12 +14,13 @@ func generate(numRows int) [][]int {
 		res[i][0] = 1
 		res[i][i] = 1
 		for j := 1; j<i ; j++{
-			res[i][j] = res[i-1][j] + res[i-1][j]
+			res[i][j] = res[i-1][j-1] + res[i-1][j]
 
 		}
 	}
 	return res
 }
+
 func main() {
 	fmt.Println(generate(5))
 	
