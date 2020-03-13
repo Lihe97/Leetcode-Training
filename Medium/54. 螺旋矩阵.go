783e79593cad27ba24fbc0962f6fd02045a2a309
Lihe97 <lihe.kansas@gmail.com>
2020-03-13 10:42:28 +0800

    螺旋矩阵

--- Medium/54. 螺旋矩阵.go
+++ Medium/54. 螺旋矩阵.go
@@ -2,70 +2,57 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
+
 
 func spiralOrder(matrix [][]int) []int {
 	res := []int{}
-	//
 	height := len(matrix)
+	width := len(matrix[0])
 	if height==0{
 		return nil
 	}
-	if height == 1{
-		return matrix[0]
-	}
-	width := len(matrix[0])
 	i , j := 0,0
-
-
-	//fmt.Println(matrix[i:height-i])
-	for i <= height-2 && j <=width-2{
-
+	for i <= int(math.Ceil(float64(height)/2-1)) && j <=int(math.Ceil(float64(width)/2-1)){
 		temp := matrix[i:height-i]
-
 		OneLayer(&res,temp,i,width - 2*j)
 		i ++
 		j++
-		//fmt.Println("resr",res)
 	}
-
-
-
-
 	return  res
-
 }
 func OneLayer(res *[]int,m [][]int,h int,w int){
-
-
 	height := len(m)
-
 	width := len(m[0])
-	//fmt.Println(w,h)
+	if w == 1{
+		for i := 0 ; i <height ; i++{
+			*res = append(*res, m[i][h])
+		}
+	}else{
 	for i := h ; i <w+h ; i ++{
 		*res = append(*res, m[0][i])
 	}
 	for i := 1  ; i < height ; i++{
-
 		*res = append(*res, m[i][w+h-1])
 	}
 	if height > 1{
-	for i := width - h-2 ; i >= h ; i--{
-		*res = append(*res, m[height-1][i])
-	}
+		for i := width - h-2 ; i >= h ; i--{
+			*res = append(*res, m[height-1][i])
+		}
 	}
 	for i := height - 2  ; i >=1 ; i--{
 		*res = append(*res, m[i][h])
+		}
 	}
-	//fmt.Println(res)
-
-
 }
 
+
 func main() {
-	//a := [][]int{{1,2,3,4},{2,3,4,5}}
-	a := [][]int{{1},{5}}
-	//fmt.Println(a)
+	//a := [][]int{{1,2,3},{4,5,6},{7,8,9}}
+	//a := [][]int{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}}
+	//a := [][]int{{2,3,4},{5,6,7},{8,9,10},{11,12,13},{14,15,16}}
+	a:=[][]int{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}}
 	fmt.Println(spiralOrder(a))
 	
 }
