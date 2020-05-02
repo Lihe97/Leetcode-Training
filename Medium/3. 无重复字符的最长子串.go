15cab0aeefd325307aa61a55f9bab1219a9c2b8d
Lihe97 <lihe.kansas@gmail.com>
2020-05-02 17:13:26 +0800

    5.2 17:13

--- Medium/3. 无重复字符的最长子串.go
+++ Medium/3. 无重复字符的最长子串.go
@@ -2,28 +2,60 @@ package main
 
 import "fmt"
 
-func lengthOfLongestSubstring(s string) int {
+//func lengthOfLongestSubstring(s string) int {
+//
+//	max := 0
+//
+//	temp := []byte{}
+//	temp = append(temp, s[0])
+//	for i := 1 ; i < len(s) ; i ++{
+//		for  j := len(temp)-1; j >= 0 ; j --{
+//			if temp[j] == s[i]{
+//				temp = temp[j+1:]
+//				break
+//			}
+//		}
+//		temp = append(temp, s[i])
+//		if len(temp) > max{
+//			max = len(temp)
+//		}
+//	}
+//	return max
+//}
+func lengthOfLongestSubstring(s string) int{
+	mp := map[byte]int{}
 
-	max := 0
+	res := 1
+	if s ==""{
+		return 0
+	}
 
-	temp := []byte{}
-	temp = append(temp, s[0])
-	for i := 1 ; i < len(s) ; i ++{
-		for  j := len(temp)-1; j >= 0 ; j --{
-			if temp[j] == s[i]{
-				temp = temp[j+1:]
-				break
+	left ,right := 0,0
+
+	mp[s[0]] = 0
+
+	for i := 1 ; i < len(s) ; i++{
+		if a,ok := mp[s[i]] ;ok{
+			if a < left{
+			}else{
+				left = mp[s[i]] + 1
 			}
+
+			mp[s[i]] = i
+			right++
+		}else{
+			mp[s[i]] = i
+			right ++
 		}
-		temp = append(temp, s[i])
-		if len(temp) > max{
-			max = len(temp)
+		if right - left + 1> res{
+			res = right - left + 1
 		}
 	}
-	return max
+	return res
+
 }
 func main() {
 
-	fmt.Println(lengthOfLongestSubstring("pwwlew"))
+	fmt.Println(lengthOfLongestSubstring("a"))
 	
 }
