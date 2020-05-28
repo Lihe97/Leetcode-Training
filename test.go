966664c67fa69a9ee8691116b4011c5f0ba6b52f
Lihe97 <lihe.kansas@gmail.com>
2020-05-28 23:36:06 +0800

    5.28 23:36

--- test.go
+++ test.go
@@ -5,6 +5,19 @@ import (
 	"strings"
 )
 
+func canWin(n int) bool {
+
+	flag := make([]bool,n+1)
+	flag[0] = false
+	for i := 1 ; i <= n ; i++{
+		flag[i] = false
+		for j := 1 ; j <= 3 && i - j >= 0 && flag[i] == false; j++{
+			flag[j] = !flag[i-j]
+		}
+	}
+	return flag[n]
+}
+
 func main() {
 
 
