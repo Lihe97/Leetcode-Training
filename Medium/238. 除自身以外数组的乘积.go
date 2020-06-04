0da05781d4778526543267d456e7f45474093c6a
Lihe97 <lihe.kansas@gmail.com>
2020-06-04 16:59:12 +0800

    6.4 16:59

--- Medium/238. 除自身以外数组的乘积.go
+++ Medium/238. 除自身以外数组的乘积.go
@@ -2,30 +2,40 @@ package main
 
 import "fmt"
 
+//func productExceptSelf(nums []int) []int {
+//
+//	left := make([]int,len(nums))
+//	left[0] = 1
+//	for  i := 1 ; i < len(nums);i++{
+//		left[i] = nums[i-1] * left[i-1]
+//	}
+//	right := make([]int,len(nums))
+//	right[len(nums)-1] = 1
+//	for j := len(nums) -2 ; j >= 0 ; j --{
+//		right[j] = nums[j+1] * right[j+1]
+//	}
+//	res := make([]int,len(nums))
+//	for i := 0 ; i < len(nums) ; i++{
+//		res[i] = left[i] * right[i]
+//	}
+//
+//	return res
+//
+//}
 func productExceptSelf(nums []int) []int {
 
-	dp := [][]int{}
+
 	res := make([]int,len(nums))
-	for i := 0 ; i < len(nums) ; i++{
-		temp := make([]int,len(nums))
-		dp = append(dp, temp)
+	res[0] = 1
+	for  i := 1 ; i < len(nums);i++{
+		res[i] = nums[i-1] * res[i-1]
 	}
-	for i := 0 ; i < len(nums) ; i++{
-		dp[i][i] = nums[i]
+	right := make([]int,len(nums))
+	right[len(nums)-1] = 1
+	for j := len(nums) -2 ; j >= 0 ; j --{
+		right[j] = nums[j+1] * right[j+1]
+		res[j] = res[j] * right[j]
 	}
-	for i := 0 ; i < len(nums) ; i++{
-
-		for j := i + 1 ;j < len(nums) ; j++{
-			dp[i][j] = nums[j] * dp[i][j-1]
-		}
-	}
-	res[0] = dp[1][len(nums)-1]
-	res[len(nums)-1] = dp[0][len(nums)-2]
-	for i := 1 ; i < len(nums) - 1 ; i++{
-		res[i] = dp[0][i-1] * dp[i+1][len(nums)-1]
-	}
-
-	//fmt.Println(res)
 	return res
 }
 func main() {
