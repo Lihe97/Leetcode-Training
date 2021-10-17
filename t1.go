5369a8acbd629167732b3fa5343f572e8ecb0d7d
Lihe97 <lihe.kansas@gmail.com>
2021-10-17 16:03:01 +0800

    10.17 16:03

--- t1.go
+++ t1.go
@@ -1,6 +1,38 @@
 package main
 
+import "fmt"
+
+func canBeIncreasing(nums []int) bool {
+
+	cnt := 0
+	flag := false
+	for i := 0 ; i <= len(nums) - 1  ;i ++{
+		temp := make([]int,len(nums[:i]))
+		copy(temp,nums[:i])
+
+		temp = append(temp,nums[i+1:]...)
+		flag = isDz(temp,len(nums)-1)
+		//fmt.Println(flag,cnt)
+		if flag {
+			cnt ++
+		}
+	}
+	return cnt >= 1
+
+}
+func isDz(a []int,n int)bool{
+	if n == 0 || n == 1{
+		return true
+	}
+	if n == 2 {
+		return a[n-1] > a[n-2]
+	}
+	return isDz(a,n-1) && (a[n-1] > a[n-2])
+}
+
 func main(){
 
+	a := []int{2,1,3}
+	fmt.Println(canBeIncreasing(a))
 
 }
--- t4.go
+++ t4.go
@@ -1,9 +1,7 @@
 package main
 
 
-
 func main()  {
-
 
 
 }
